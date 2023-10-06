4d8f75a977f0aca8a4fbc86c87179f5933174186
tinjohn <tina.john@th-luebeck.de>
2023-10-06 10:25:53 +0200

    UX things

--- lang/en/isymetaselectact.php
+++ lang/en/isymetaselectact.php
@@ -23,56 +23,36 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['clicktoopen'] = 'Click on {$a} to open the resource.';
-$string['configdisplayoptions'] = 'Select all options that should be available, existing settings are not modified. Hold CTRL key to select multiple fields.';
-$string['configframesize'] = 'When a web page or an uploaded file is displayed within a frame, this value is the height (in pixels) of the top frame (which contains the navigation).';
-$string['configrolesinparams'] = 'Should customised role names (from the course settings) be available as variables for URL parameters?';
-$string['configsecretphrase'] = 'This secret phrase is used to produce encrypted code value that can be sent to some servers as a parameter.  The encrypted code is produced by an md5 value of the current user IP address concatenated with your secret phrase. ie code = md5(IP.secretphrase). Please note that this is not reliable because IP address may change and is often shared by different computers.';
 $string['contentheader'] = 'Content';
-$string['createisymetaselectact'] = 'Create a URL';
+$string['createisymetaselectact'] = 'Create an ISy Course Overview Activity ';
 
-$string['coursedesc'] = 'Querystring';
+$string['coursedesc'] = 'Search term';
 
-$string['indicator:cognitivedepth'] = 'URL cognitive';
-$string['indicator:cognitivedepth_help'] = 'This indicator is based on the cognitive depth reached by the student in a URL resource.';
-$string['indicator:cognitivedepthdef'] = 'URL cognitive';
-$string['indicator:cognitivedepthdef_help'] = 'The participant has reached this percentage of the cognitive engagement offered by the URL resources during this analysis interval (Levels = No view, View)';
+$string['indicator:cognitivedepth'] = 'ISy Course Overview Activity cognitive';
+$string['indicator:cognitivedepth_help'] = 'This indicator is based on the cognitive depth reached by the student in a ISy Course Overview Activity resource.';
+$string['indicator:cognitivedepthdef'] = 'ISy Course Overview Activity cognitive';
+$string['indicator:cognitivedepthdef_help'] = 'The participant has reached this percentage of the cognitive engagement offered by the ISy Course Overview Activity resources during this analysis interval (Levels = No view, View)';
 $string['indicator:cognitivedepthdef_link'] = 'Learning_analytics_indicators#Cognitive_depth';
-$string['indicator:socialbreadth'] = 'URL social';
-$string['indicator:socialbreadth_help'] = 'This indicator is based on the social breadth reached by the student in a URL resource.';
-$string['indicator:socialbreadthdef'] = 'URL social';
+$string['indicator:socialbreadth'] = 'ISy Course Overview Activity social';
+$string['indicator:socialbreadth_help'] = 'This indicator is based on the social breadth reached by the student in a ISy Course Overview Activity resource.';
+$string['indicator:socialbreadthdef'] = 'ISy Course Overview Activity  social';
 $string['indicator:socialbreadthdef_help'] = 'The participant has reached this percentage of the social engagement offered by the URL resources during this analysis interval (Levels = No participation, Participant alone)';
 $string['indicator:socialbreadthdef_link'] = 'Learning_analytics_indicators#Social_breadth';
 
-$string['modulename'] = 'ISy Course View Activity';
-$string['modulename_help'] = 'The URL module enables a teacher to provide a web link as a course resource. Anything that is freely available online, such as documents or images, can be linked to; the URL doesn’t have to be the home page of a website. The URL of a particular web page may be copied and pasted or a teacher can use the file picker and choose a link from a repository such as Flickr, YouTube or Wikimedia (depending upon which repositories are enabled for the site).
-
-There are a number of display options for the URL, such as embedded or opening in a new window and advanced options for passing information, such as a student\'s name, to the URL if required.
-
-Note that URLs can also be added to any other resource or activity type through the text editor.';
+$string['modulename'] = 'ISy Course Overview Activity';
+$string['modulename_help'] = 'The ISy Course Overview Activity module enables a teacher to provide a course overview within a course to recommend other courses by search term and other filters offered by ISy Metaselect, based on the ISy Meta\'s metadata given for the courses on the platform.';
 $string['modulename_link'] = 'mod/isymetaselectact/view';
-$string['modulenameplural'] = 'URLs';
+$string['modulenameplural'] = 'ISy Course Overview Activities';
 $string['name'] = 'Name';
-$string['name_help'] = 'This will serve as the link text for the URL.
-
-Enter a meaningful text that concisely describes the URL\'s purpose.
-
-Avoid using the word "link". This will help screen reader users as screen readers announce links (e.g. "Moodle.org, link") so there\'s no need to include the word "link" in the name field.';
-$string['page-mod-isymetaselectact-x'] = 'Any URL module page';
-$string['parameterinfo'] = '&amp;parameter=variable';
-$string['parametersheader'] = 'URL variables';
-$string['parametersheader_help'] = 'This section allows you to pass internal information as part of the URL. This is useful if the URL is an interactive web page that takes parameters, and you want to pass something like the name of the current user, for example. Enter the name of the URL\'s parameter in the text box then select the corresponding site variable.';
+$string['name_help'] = 'The name for the activity and the ISy Course Overview Activity filter';
+$string['page-mod-isymetaselectact-x'] = 'Any ISy Course Overview Activity module page';
 $string['pluginadministration'] = 'Isy Course View Activity module administration';
-$string['pluginname'] = 'ISy Course View Activity';
-$string['popupheight'] = 'Pop-up height (in pixels)';
-$string['popupheightexplain'] = 'Specifies default height of popup windows.';
-$string['popupwidth'] = 'Pop-up width (in pixels)';
-$string['popupwidthexplain'] = 'Specifies default width of popup windows.';
+$string['pluginname'] = 'ISy Course Overview Activity';
 $string['printintro'] = 'Display Isy Course View Activity description';
 $string['printintroexplain'] = 'Display Isy Course View Activity description below content? Some display types may not display description even if enabled.';
 $string['privacy:metadata'] = 'The Isy Course View Activity resource plugin does not store any personal data.';
 $string['rolesinparams'] = 'Role names as Isy Course View Activity variables';
-$string['search:activity'] = 'ISy Course View Activity';
+$string['search:activity'] = 'ISy Course Overview Activity';
 $string['serverurl'] = 'Server URL';
-$string['isymetaselectact:addinstance'] = 'Add a new ISy Course View Activity resource';
-$string['isymetaselectact:view'] = 'View ISy Course View Activity';
+$string['isymetaselectact:addinstance'] = 'Add a new ISy Course Overview Activity resource';
+$string['isymetaselectact:view'] = 'View ISy Course Overview Activity';
